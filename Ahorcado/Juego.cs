e095099a07723b5bb9b793940a98b6377b9df5a4
LENOVO <LENOVO@DESKTOP-7SL9R98>
2025-03-14 12:19:00 -0400

    Agregar archivos de proyecto.

---
+++ Ahorcado/Juego.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahorcado
+{
+    class Juego
+    {
+        private Palabras palabra;
+        private int intentos;
+
+        public Juego()
+        {
+            palabra = new Palabras();
+            intentos = 3;
+        }
+
+        
+        public void setIntentos()
+        {
+            intentos = intentos - 1;
+        }
+        
+        
+
+        public void jugar()
+        {
+            string palabraAadivinar = palabra.escogerPalabra().ToLower();
+            string descripcion = palabra.descripcion();
+            string[] palabraAdivinada = new string[palabraAadivinar.Length];
+            
+            Console.WriteLine(descripcion);
+            for (int i = 0; i < palabraAadivinar.Length; i++)
+            {
+                palabraAdivinada[i] = "_";
+
+            }
+
+
+            
+            
+            while (true)
+            {
+                Console.WriteLine("Intentos: " + intentos);
+                Console.WriteLine("Letra: ");
+                Console.WriteLine(string.Join(" ", palabraAdivinada));
+                string entrada = Console.ReadLine().ToLower();
+                char letra = entrada[0];
+                bool letraEncontrada = false;
+                for (int i = 0; i < palabraAadivinar.Length; i++)
+                {
+
+                    if (palabraAadivinar[i] == letra)
+                    {
+                        palabraAdivinada[i] = entrada;
+                        letraEncontrada = true;
+                    }
+                    
+                }
+                if (!letraEncontrada)
+                {
+                    setIntentos();
+                }
+
+                if(string.Join("",palabraAdivinada) == palabraAadivinar)
+                {
+                    Console.WriteLine(string.Join(" ", palabraAdivinada));
+                    Console.WriteLine("Ganaste el juego");
+                    break;
+                }
+
+                if(intentos == 0)
+                {
+                    Console.WriteLine("Se te acabo el juego");
+                    Console.WriteLine("La palabra era: " + palabraAadivinar);
+                    break;
+
+                }
+
+            }
+        }
+
+    }
+}
---
+++ Ahorcado/Palabras.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahorcado
+{
+    class Palabras
+    {
+        private string[] bancodePalabras = { "Argentina", "Brasil", "Colegio", "Dentista", "Futbol", "Universidad" };
+        private string[] descripciones = {
+            "País en Sudamérica conocido por su tango y fútbol.",
+            "País en Sudamérica famoso por su Carnaval y la Amazonía.",
+            "Institución educativa donde se enseña desde primaria hasta secundaria.",
+            "Profesional de la salud que cuida los dientes y la boca.",
+            "Deporte que se juega entre dos equipos de 11 jugadores con una pelota.",
+            "Institución educativa de nivel superior, donde se obtienen grados académicos."
+         };
+        public int generarNumeroRandom()
+        {
+            Random numero = new Random();
+            int numeroRandom = numero.Next(bancodePalabras.Length);
+            return numeroRandom;
+
+        }
+        public string escogerPalabra()
+        {
+            string palabraElegida = bancodePalabras[generarNumeroRandom()];
+            
+            return palabraElegida;
+        }
+        public string descripcion()
+        {
+            string descripcionPalabra = descripciones[generarNumeroRandom()];
+            return descripcionPalabra;
+        }
+
+        
+
+
+    }
+}
